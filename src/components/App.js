d7508b31b4b3b09b5fa49693a9762b147c0b4b70
username <storyno7@gmail.com>
2021-12-12 04:01:53 +0900

    🚧Logging in to Firebase.

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import AppRouter from 'components/Router'
+import { authService } from 'myBase'
+
+function App() {
+  console.log(authService.currentUser)
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser)
+  return <AppRouter isLoggedIn={isLoggedIn} />
+}
+
+export default App
