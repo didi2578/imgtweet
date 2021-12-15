6e3d8e6d434e3983cc1fdd0e4e454cebcca4adc8
username <storyno7@gmail.com>
2021-12-15 15:48:52 +0900

    ✨Create my tweet component on my page.

---
+++ src/routes/MyTweets.js
@@ -0,0 +1,39 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { dbService } from 'myBase'
+import { collection, getDocs, query, where, orderBy } from 'firebase/firestore'
+import ImgTweet from 'components/ImgTweet'
+
+const MyTweets = ({ userObj }) => {
+  const [myTweets, setMyTweets] = useState([])
+  const getMyTweets = useCallback(async () => {
+    const q = query(
+      collection(dbService, 'tweets'),
+      where('creatorId', '==', userObj.uid),
+      orderBy('createdAt', 'desc')
+    )
+
+    const querySnapshot = await getDocs(q)
+
+    const tweetArr = querySnapshot.docs.map((doc) => doc.data())
+    setMyTweets(tweetArr)
+  }, [userObj])
+
+  useEffect(() => {
+    getMyTweets()
+  }, [getMyTweets])
+  console.log(myTweets)
+
+  return (
+    <div>
+      {myTweets.map((myTweet, index) => (
+        <ImgTweet
+          key={index}
+          userObj={myTweet}
+          isOwner={myTweet.creatorId === userObj.uid}
+        />
+      ))}
+    </div>
+  )
+}
+
+export default MyTweets
